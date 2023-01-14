5d3494d0af68c32e8a645a20be205d839c33d4cd
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2023-01-14 14:22:07 +0100

    Backprop in DAG

--- src/main.zig
+++ src/main.zig
@@ -97,25 +97,41 @@ const DAG = struct {
         self.edges.deinit();
     }
 
-    pub fn constant(self: *Self, value: i32) !NodeHandle {
+    pub fn constant(self: *Self, value: f32) !NodeHandle {
         var handle: NodeHandle = self.nodes.items.len;
-        try self.nodes.append(Node{ .name = "test", .op = .Constant, .value = value });
+        try self.nodes.append(Node{ .name = "constant", .op = .Constant, .value = value, .grad = 0.0 });
         return handle;
     }
 
     pub fn add(self: *Self, a: NodeHandle, b: NodeHandle) !NodeHandle {
         var handle: NodeHandle = self.nodes.items.len;
-        try self.nodes.append(Node{ .name = "add", .op = .Addition, .value = null });
+        try self.nodes.append(Node{ .name = "add", .op = .Addition, .value = null, .grad = 0.0 });
         try self.edges.append(DirectedEdge{ .from = a, .to = handle });
         try self.edges.append(DirectedEdge{ .from = b, .to = handle });
         return handle;
     }
 
     pub fn sub(self: *Self, a: NodeHandle, b: NodeHandle) !NodeHandle {
+        // TODO: This can reuse Mul and Add. add(self, mul(-1, other))
         var handle = self.nodes.items.len;
-        try self.nodes.append(Node{ .name = "sub", .op = .Subtraction, .value = null });
+        try self.nodes.append(Node{ .name = "sub", .op = .Subtraction, .value = null, .grad = 0.0 });
         try self.edges.append(DirectedEdge{ .from = a, .to = handle });
         try self.edges.append(DirectedEdge{ .from = b, .to = handle });
+        return handle;
+    }
+
+    pub fn mul(self: *Self, a: NodeHandle, b: NodeHandle) !NodeHandle {
+        var handle = self.nodes.items.len;
+        try self.nodes.append(Node{ .name = "mul", .op = .Multiplication, .value = null, .grad = 0.0 });
+        try self.edges.append(DirectedEdge{ .from = a, .to = handle });
+        try self.edges.append(DirectedEdge{ .from = b, .to = handle });
+        return handle;
+    }
+
+    pub fn tanh(self: *Self, a: NodeHandle) !NodeHandle {
+        var handle = self.nodes.items.len;
+        try self.nodes.append(Node{ .name = "tanh", .op = .TanH, .value = null, .grad = 0.0 });
+        try self.edges.append(DirectedEdge{ .from = a, .to = handle });
         return handle;
     }
 
@@ -130,34 +146,44 @@ const DAG = struct {
             var current_node = &self.nodes.items[current_node_handle];
             std.debug.print("EVALUATING {s}, {?}\n", .{ current_node.name, current_node });
 
+            // Limit to 2 incoming edges to a given node.
+            var in_a: ?*const Node = null;
+            var in_b: ?*const Node = null;
+            for (self.edges.items) |edge| {
+                if (edge.to != current_node_handle) continue;
+                const incomming_node = &self.nodes.items[edge.from];
+                std.debug.print("Incoming: {s} -> {s}\n", .{ incomming_node.name, current_node.name });
+                if (in_a == null) {
+                    in_a = incomming_node;
+                } else if (in_b == null) {
+                    in_b = incomming_node;
+                } else {
+                    std.debug.print("One too many incoming nodes to {s} -> {s}: {?}: {?}", .{ incomming_node.name, current_node.name, incomming_node, current_node });
+                    assert(false); // We only allow 2 incoming edges to a node.
+                }
+            }
+
+            // TODO: This check should not be performed for unary ops.
+            if (current_node.op != .Constant and current_node.op != .TanH and (in_a == null or in_b == null)) {
+                std.debug.print("Not enough inputs to Op node {s}: {?}", .{ current_node.name, current_node });
+                assert(false); // Not enough inputs to op node.
+            }
+
             switch (current_node.op) {
                 .Constant => {
                     // do nothing, value should already be set.
                 },
                 .Addition => {
-                    // Get the input edges to this node, they should already be evaluated.
-                    var sum: i32 = 0;
-                    for (self.edges.items) |edge| {
-                        if (edge.to != current_node_handle) continue;
-                        std.debug.print("\t {?}\n", .{edge});
-                        sum += self.nodes.items[edge.from].value.?;
-                    }
-                    current_node.value = sum;
+                    OpAddition.forward(current_node, in_a.?, in_b.?);
                 },
                 .Subtraction => {
-                    // Get the input edges to this node, they should already be evaluated.
-                    var result: ?i32 = null;
-                    for (self.edges.items) |edge| {
-                        if (edge.to != current_node_handle) continue;
-                        var value = self.nodes.items[edge.from].value.?;
-                        if (result == null) {
-                            result = value;
-                        } else {
-                            result = result.? - value;
-                        }
-                        std.debug.print("s {?}\n", .{result});
-                    }
-                    current_node.value = result;
+                    OpSubtraction.forward(current_node, in_a.?, in_b.?);
+                },
+                .Multiplication => {
+                    OpMultiplication.forward(current_node, in_a.?, in_b.?);
+                },
+                .TanH => {
+                    OpTanH.forward(current_node, in_a.?);
                 },
             }
             std.debug.print("DONE EVALUATING {s}, {?}\n", .{ current_node.name, current_node });
@@ -167,6 +193,62 @@ const DAG = struct {
         for (sorted_nodes.items) |current_node_handle| {
             var node = self.nodes.items[current_node_handle];
             std.debug.print("{?}\n", .{node});
+        }
+    }
+    pub fn resolveNodeBackward(self: *Self, node_handle: NodeHandle) !void {
+        var sorted_nodes = try self.toposort_dfs(node_handle);
+        defer sorted_nodes.deinit();
+
+        // Set initial grad
+        // TODO, reset all grads in graph.
+        self.nodes.items[node_handle].grad = 1.0;
+
+        var i: usize = 0;
+        while (i < sorted_nodes.items.len) : (i += 1) {
+            var current_node_handle = sorted_nodes.items[i];
+            var current_node = &self.nodes.items[current_node_handle];
+            std.debug.print("BACKPROPPING {s}, {?}\n", .{ current_node.name, current_node });
+
+            // Limit to 2 incoming edges to a given node.
+            var in_a: ?*Node = null;
+            var in_b: ?*Node = null;
+            for (self.edges.items) |edge| {
+                if (edge.to != current_node_handle) continue;
+                const incomming_node = &self.nodes.items[edge.from];
+                std.debug.print("Incoming: {s} -> {s}\n", .{ incomming_node.name, current_node.name });
+                if (in_a == null) {
+                    in_a = incomming_node;
+                } else if (in_b == null) {
+                    in_b = incomming_node;
+                } else {
+                    std.debug.print("One too many incoming nodes to {s} -> {s}: {?}: {?}", .{ incomming_node.name, current_node.name, incomming_node, current_node });
+                    assert(false); // We only allow 2 incoming edges to a node.
+                }
+            }
+
+            // TODO: This check should not be performed for unary ops.
+            if (current_node.op != .Constant and current_node.op != .TanH and (in_a == null or in_b == null)) {
+                std.debug.print("Not enough inputs to Op node {s}: {?}", .{ current_node.name, current_node });
+                assert(false); // Not enough inputs to op node.
+            }
+
+            switch (current_node.op) {
+                .Constant => {
+                    // do nothing, value should already be set.
+                },
+                .Addition => {
+                    OpAddition.backward(current_node, in_a.?, in_b.?);
+                },
+                .Subtraction => {
+                    OpSubtraction.backward(current_node, in_a.?, in_b.?);
+                },
+                .Multiplication => {
+                    OpMultiplication.backward(current_node, in_a.?, in_b.?);
+                },
+                .TanH => {
+                    OpTanH.backward(current_node, in_a.?);
+                },
+            }
         }
     }
 
@@ -205,14 +287,102 @@ const DAG = struct {
         return sorted_nodes;
     }
 
-    const Op = enum { Constant, Addition, Subtraction };
+    const Op = enum { Constant, Addition, Subtraction, Multiplication, TanH };
     const NodeHandle = u64;
     const DirectedEdge = struct { from: NodeHandle, to: NodeHandle };
     const Node = struct {
         name: []const u8,
         op: Op,
-        value: ?i32, // to be tensor?
+        value: ?f32, // to be tensor?
+        grad: f32, // to be tensor?
     };
+
+    const OpInterface = struct {
+        // Limited to only 1 other value in
+        forward: *const fn (*f32, *const f32) void,
+        backward: *const fn (*f32, *const f32) void,
+    };
+};
+
+const OpAddition = struct {
+    fn forward(out: *DAG.Node, in_a: *const DAG.Node, in_b: *const DAG.Node) void {
+        out.value = in_a.value.? + in_b.value.?;
+    }
+
+    fn backward(out: *DAG.Node, in_a: *DAG.Node, in_b: *DAG.Node) void {
+        in_a.grad += 1.0 * out.grad;
+        in_b.grad += 1.0 * out.grad;
+    }
+};
+test "OpAddition forward/backward" {
+    var out = DAG.Node{ .name = "", .op = .Addition, .value = null, .grad = 0.5 };
+    var in_a = DAG.Node{ .name = "", .op = .Constant, .value = 2.0, .grad = 0.0 };
+    var in_b = DAG.Node{ .name = "", .op = .Constant, .value = 3.0, .grad = 0.0 };
+    OpAddition.forward(&out, &in_a, &in_b);
+    try std.testing.expectEqual(out.value, 5.0);
+
+    OpAddition.backward(&out, &in_a, &in_b);
+    try std.testing.expectEqual(out.grad, 0.5);
+    try std.testing.expectEqual(in_a.grad, 0.5);
+    try std.testing.expectEqual(in_b.grad, 0.5);
+}
+
+const OpSubtraction = struct {
+    fn forward(out: *DAG.Node, in_a: *const DAG.Node, in_b: *const DAG.Node) void {
+        out.value = in_a.value.? - in_b.value.?;
+    }
+
+    fn backward(out: *DAG.Node, in_a: *DAG.Node, in_b: *DAG.Node) void {
+        in_a.grad += 1.0 * out.grad;
+        in_b.grad += 1.0 * out.grad;
+    }
+};
+test "OpSubtraction forward/backward" {
+    var out = DAG.Node{ .name = "", .op = .Addition, .value = null, .grad = 0.5 };
+    var in_a = DAG.Node{ .name = "", .op = .Constant, .value = 2.0, .grad = 0.0 };
+    var in_b = DAG.Node{ .name = "", .op = .Constant, .value = 3.0, .grad = 0.0 };
+    OpSubtraction.forward(&out, &in_a, &in_b);
+    try std.testing.expectEqual(out.value, -1.0);
+
+    OpSubtraction.backward(&out, &in_a, &in_b);
+    try std.testing.expectEqual(out.grad, 0.5);
+    try std.testing.expectEqual(in_a.grad, 0.5);
+    try std.testing.expectEqual(in_b.grad, 0.5);
+}
+
+const OpMultiplication = struct {
+    fn forward(out: *DAG.Node, in_a: *const DAG.Node, in_b: *const DAG.Node) void {
+        out.value = in_a.value.? * in_b.value.?;
+    }
+
+    fn backward(out: *DAG.Node, in_a: *DAG.Node, in_b: *DAG.Node) void {
+        in_a.grad += in_b.value.? * out.grad;
+        in_b.grad += in_a.value.? * out.grad;
+    }
+};
+test "OpMultiplication forward/backward" {
+    var out = DAG.Node{ .name = "", .op = .Multiplication, .value = null, .grad = 1.0 };
+    var in_a = DAG.Node{ .name = "", .op = .Constant, .value = 2.0, .grad = 0.0 };
+    var in_b = DAG.Node{ .name = "", .op = .Constant, .value = 3.0, .grad = 0.0 };
+    OpMultiplication.forward(&out, &in_a, &in_b);
+    try std.testing.expectEqual(out.value, 6.0);
+
+    OpMultiplication.backward(&out, &in_a, &in_b);
+    try std.testing.expectEqual(out.grad, 1.0);
+    try std.testing.expectEqual(in_a.grad, 3.0);
+    try std.testing.expectEqual(in_b.grad, 2.0);
+}
+
+const OpTanH = struct {
+    fn forward(out: *DAG.Node, in_a: *const DAG.Node) void {
+        const v = in_a.value.?;
+        out.value = (std.math.exp(2.0 * v) - 1) / (std.math.exp(2.0 * v) + 1);
+    }
+
+    fn backward(out: *DAG.Node, in_a: *DAG.Node) void {
+        const t = out.value.?;
+        in_a.grad += (1.0 - t * t) * out.grad;
+    }
 };
 
 pub fn main() !void {
@@ -235,30 +405,20 @@ pub fn main() !void {
     // - Get inspiration from ECS videos on how to get the handles..
     // - actual Backprop
 
-    var device_cpu = TensorDeviceCPU.init(std.heap.page_allocator);
-    defer device_cpu.deinit();
-
-    var tensor = try device_cpu.createTensor(TensorDescriptor{
-        .dimensions_sizes = &[_]usize{ 256, 512 },
-    });
-    _ = tensor;
-
-    var tensor_a = try device_cpu.createTensor(TensorDescriptor{
-        .dimensions_sizes = &[_]usize{ 1, 1 },
-    });
-    device_cpu.setTensorData(tensor_a, &[_]f32{1.0});
-
-    var tensor_b = try device_cpu.createTensor(TensorDescriptor{
-        .dimensions_sizes = &[_]usize{ 1, 1 },
-    });
-    device_cpu.setTensorData(tensor_b, &[_]f32{2.0});
-
-    device_cpu.tensorOpAddition(tensor_a, tensor_b);
-    var values = device_cpu.tensorValues(tensor_a);
-    std.debug.print("Values: {any}", .{values});
+    // var device_cpu = TensorDeviceCPU.init(std.heap.page_allocator);
+    // defer device_cpu.deinit();
 
     // Goal 1 recreate micrograd
-    //
+    // Dag needs to support backprop
+    // Neuron takes # of inputs that come to a neuron
+    // When, created a random weight is created for each input [0, 1]
+    // and a bias for the neuron [0, 1].
+    // Call neuron with input x:
+    //  act = w.dot(x) + b
+    //  return act.tanh()
+    // Next, a layer of neurons is defined (MLP)
+    // The concepts above should define these operations in terms of the DAG primitives,
+    // so that we can back propagate later.
 
     // Goal 2 is to be able to train and run MNIST dataset
     // Ops needed:
@@ -284,24 +444,79 @@ pub fn main() !void {
 
 }
 
-test "dag test" {
+test "dag test forward/backward f = (1+2) - 6" {
     var graph = DAG.init(std.testing.allocator);
     defer graph.deinit();
-    var a = try graph.constant(1);
-    var b = try graph.constant(2);
+
+    // f = (1+2) - 6
+    var a = try graph.constant(1.0);
+    var b = try graph.constant(2.0);
     var sum = try graph.add(a, b);
-    var c = try graph.constant(6);
+    var c = try graph.constant(6.0);
     var sub = try graph.sub(sum, c);
-    try graph.resolveNode(sub);
-    try std.testing.expectEqual(graph.nodes.items[sub].value, -3);
+    var out_node_handle = sub;
+
+    // Forward
+    try graph.resolveNode(out_node_handle);
+    try std.testing.expectEqual(graph.nodes.items[out_node_handle].value, -3.0);
+
+    // Backward
+    // try graph.resolveNodeBackward(out_node_handle);
+
+    // MPS does it like this:
+    // torch does it like this:
+    //  logit1 = torch.Tensor([1.0]).double() ; logit4.requires_grad = True
+    //  ...
+    //  logits = [logit1, logit2, logit3, logit4]
+    //  probs = softmax(logits)
+    //  loss = -probs[3].log()
+    //  loss.backward()
+}
+
+test "dag test forward/backward f = tanh(2*(-3) + (0*1) + 6.7)" {
+    var graph = DAG.init(std.testing.allocator);
+    defer graph.deinit();
+
+    // f = tanh(2*(-3) + (0*1) + 6.7)
+    var a = try graph.constant(2.0);
+    var b = try graph.constant(-3.0);
+    var mul1 = try graph.mul(a, b);
+    var c = try graph.constant(0.0);
+    var d = try graph.constant(1.0);
+    var mul2 = try graph.mul(c, d);
+    var sum1 = try graph.add(mul1, mul2);
+    var e = try graph.constant(6.8813735870195432);
+    var sum2 = try graph.add(sum1, e);
+    var tanh = try graph.tanh(sum2);
+    var out_node_handle = tanh;
+
+    // Forward
+    try graph.resolveNode(out_node_handle);
+    try std.testing.expectApproxEqAbs(graph.nodes.items[out_node_handle].value.?, 0.7071, 0.001);
+
+    // Backward
+    try graph.resolveNodeBackward(out_node_handle);
+    try std.testing.expectApproxEqAbs(graph.nodes.items[c].grad, 0.5, 0.001);
+    try std.testing.expectApproxEqAbs(graph.nodes.items[d].grad, 0.0, 0.001);
+    try std.testing.expectApproxEqAbs(graph.nodes.items[a].grad, -1.5, 0.001);
+    try std.testing.expectApproxEqAbs(graph.nodes.items[b].grad, 1.0, 0.001);
 }
 
 test "tensor test" {
     var device_cpu = TensorDeviceCPU.init(std.testing.allocator);
     defer device_cpu.deinit();
 
-    var tensor = try device_cpu.createTensor(TensorDescriptor{
-        .dimensions_sizes = &[_]usize{ 256, 512 },
+    var tensor_a = try device_cpu.createTensor(TensorDescriptor{
+        .dimensions_sizes = &[_]usize{ 1, 1 },
     });
-    _ = tensor;
+    device_cpu.setTensorData(tensor_a, &[_]f32{1.0});
+
+    var tensor_b = try device_cpu.createTensor(TensorDescriptor{
+        .dimensions_sizes = &[_]usize{ 1, 1 },
+    });
+    device_cpu.setTensorData(tensor_b, &[_]f32{2.0});
+
+    device_cpu.tensorOpAddition(tensor_a, tensor_b);
+    var values = device_cpu.tensorValues(tensor_a);
+    try std.testing.expectEqual(values[0], 3.0);
 }
